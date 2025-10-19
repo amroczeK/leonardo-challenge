654521ce41f67089a46215436a2ceaeba6f5f453
amroczeK <mroczek9990@gmail.com>
2025-10-19 20:07:27 +0800

    feat: Configure apollo client for SSR pages

---
+++ src/lib/graphql/server-client.ts
@@ -0,0 +1,26 @@
+import { HttpLink } from "@apollo/client";
+import {
+  registerApolloClient,
+  ApolloClient,
+  InMemoryCache,
+} from "@apollo/client-integration-nextjs";
+
+/**
+ * Server-side Apollo Client for SSR data fetching
+ * This client is used exclusively in Server Components
+ * Reference: https://www.apollographql.com/blog/apollo-client-integration-nextjs-officially-released
+ * Reference: https://github.com/apollographql/apollo-client-integrations/tree/main/packages/nextjs
+ */
+export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
+  return new ApolloClient({
+    cache: new InMemoryCache(),
+    link: new HttpLink({
+      // Absolute URL is required for SSR
+      uri: "https://rickandmortyapi.com/graphql",
+      fetchOptions: {
+        // Disable caching for SSR to always get fresh data
+        cache: "no-store",
+      },
+    }),
+  });
+});
