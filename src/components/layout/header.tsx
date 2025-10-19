7d24dc752258dbe43920d23b102636d84bba52ec
amroczeK <mroczek9990@gmail.com>
2025-10-19 11:16:45 +0800

    feat: Add mode toggle to header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavigationBar from "./navigation-bar";
+import { ModeToggle } from "../ui/mode-toggle";
 
 function Header() {
   return (
-    <header className="container-layout py-4">
+    <header className="container-layout py-4 flex justify-between items-center">
       <NavigationBar />
+      <ModeToggle />
     </header>
   );
 }
